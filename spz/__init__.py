8435bf9d3f5b8ff3b2d3241972d3ca5f18b53a48
Daniel J. Hofmann <daniel@trvx.org>
2013-06-06 16:08:38 +0200

    Customized behavior by inheriting from the flask class (whitespace and newline control, reloading)

--- spz/__init__.py
+++ spz/__init__.py
@@ -3,8 +3,18 @@
 from flask import Flask
 
 
-app = Flask(__name__, instance_relative_config=True)
+# provides custom options by inheriting from the flask class
+class CustomFlask(Flask):
+    jinja_options = dict(Flask.jinja_options, trim_blocks=True, lstrip_blocks=True, auto_reload=False)
 
+app = CustomFlask(__name__, instance_relative_config=True)
+
+
+app.config.from_pyfile('development.cfg')
+#app.config.from_pyfile('production.cfg')
+
+
+# register all views here
 from spz import views
 from spz import errorhandlers
 
