14eb950e9bdfc9f30e3c269fa363fc071d517d76
Daniel J. Hofmann <daniel@trvx.org>
2013-06-14 15:50:51 +0200

    Implemented a custom webassets filter for rcssmin

---
+++ spz/util/RCSSMin.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Implements the RCSSMin stylesheet minifier filter for webassets.
+
+   The assets package does not provide a filter for rcssmin.
+   We probably could use cssmin, but it's not as up to date as rcssmin.
+"""
+
+from rcssmin import cssmin
+from flask.ext.assets import Filter
+
+
+class RCSSMin(Filter):
+    """Stylesheet minifier, using the RCSSmin library.
+
+       .. note: you have to register this filter explicitely before using it
+    """
+
+    name = 'rcssmin'
+
+    def output(self, _in, out, **kw):
+        out.write(cssmin(_in.read()))
+
+
+# vim: set tabstop=4 shiftwidth=4 expandtab:
---
+++ spz/util/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Utility function and classes.
+
+   Mostly independent functionality that isn't especially bound to this project.
+"""
+
+
+# vim: set tabstop=4 shiftwidth=4 expandtab:
