56ba907cf08c33820b4a66ed5f5364ef81caec9f
Daniel J. Hofmann <daniel@trvx.org>
2013-06-23 11:36:18 +0200

    Profiling and sanity checking support for local development server

--- runserver.py
+++ runserver.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import argparse
+
 from spz import app
 
 
@@ -8,6 +10,24 @@ from spz import app
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Starts a local development server running the spz application')
+
+    mode = parser.add_mutually_exclusive_group(required=False)
+    mode.add_argument('-p', '--profile', dest='profiling', action="store_true",
+                      help='Enable profiling support, see: http://werkzeug.pocoo.org/docs/contrib/profiler/')
+    mode.add_argument('-l', '--lint', dest='linting', action="store_true",
+                      help='Enable WSGI sanity checks, see: http://werkzeug.pocoo.org/docs/contrib/lint/')
+
+    args = parser.parse_args()
+
+    if args.profiling:
+        from werkzeug.contrib.profiler import ProfilerMiddleware
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app)
+
+    elif args.linting:
+        from werkzeug.contrib.lint import LintMiddleware
+        app.wsgi_app = LintMiddleware(app.wsgi_app)
+
     app.run(host='127.0.0.1', port=8080)
 
 
