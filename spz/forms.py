6ecb16a70eed5065c87d9f50fd4dc75bf7b70ffa
Roman Zukowsky <roman@zukowsky,de>
2013-06-25 16:12:47 +0200

    Deutsche Bezeichnung für Telefon und E-Mail

--- spz/forms.py
+++ spz/forms.py
@@ -19,8 +19,8 @@ from wtforms import TextField, validators
 class SignupForm(Form):
     first_name = TextField('Vorname', [validators.Length(1, 60, 'Länge muss zwischen 1 und 60 Zeichen sein')])
     last_name = TextField('Nachname', [validators.Length(1, 60, 'Länge muss zwischen 1 and 60 sein')])
-    phone = TextField('Telephon', [validators.Length(max=20, message='Länge darf maximal 20 Zeichen sein')])
-    mail = TextField('Mail', [validators.Email('Valide Mail Adresse wird benötigt'),
+    phone = TextField('Telefon', [validators.Length(max=20, message='Länge darf maximal 20 Zeichen sein')])
+    mail = TextField('E-Mail', [validators.Email('Valide Mail Adresse wird benötigt'),
                               validators.Length(max=120, message='Länge muss zwischen 1 und 120 Zeichen sein')])
     tag = TextField('Identifikation', [validators.DataRequired('Identifikation wird benötigt'),
                                        validators.Length(max=20, message='Länge darf maximal 20 Zeichen sein')])
