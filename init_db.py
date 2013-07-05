9d92c47b0c45f0071c9a612d1da4fac254a8d727
Daniel J. Hofmann <daniel@trvx.org>
2013-07-05 14:20:52 +0200

    Dummy database values for testing

--- init_db.py
+++ init_db.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime, timedelta
+
 from spz import db
 from spz.models import *
+
+
+def dummy_values():
+    fst_lang = Language('First Language', datetime.utcnow(), datetime.utcnow() + timedelta(weeks=2))
+    snd_lang = Language('Second Language', datetime.utcnow() + timedelta(days=2), datetime.utcnow() + timedelta(days=4))
+
+    c1 = Course(fst_lang, '1a', limit=15, price=60)
+    c2 = Course(fst_lang, '1b', limit=10, price=60)
+    c3 = Course(snd_lang, '2a', limit=15, price=60)
+    c4 = Course(snd_lang, '2b', limit=15, price=120)
+    db.session.add_all([c1, c2, c3, c4])
+
+    bsc = Degree('Bachelor')
+    msc = Degree('Master')
+    db.session.add_all([bsc, msc])
+
+    o1 = Origin('First origin')
+    o2 = Origin('Second origin', 'Department')
+    db.session.add_all([o1, o2])
+
+    db.session.commit()
 
 
 # Has to be done only once, to initialize the database;
@@ -9,6 +32,8 @@ from spz.models import *
 
 if __name__ == '__main__':
     db.create_all()
+
+    dummy_values()
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:
