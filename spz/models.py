spz-signup/spz-signup

commit d71e225d56e86f98459fce6caa1d8d3b87223610

Restock functionality for languages

spz/models.py
@@ -188,22 +188,35 @@ class Course(db.Model):
         return self.rating_lowest <= applicant.best_rating() <= self.rating_highest
 
     def is_full(self):
-        return len(self.attendances) >= self.limit
+        return len(self.get_active_attendances()) >= self.limit
 
     def is_overbooked(self):
         return len(self.attendances) >= (self.limit * app.config['OVERBOOKING_FACTOR'])
 
-    def get_waiting_applicants(self):
+    def get_waiting_attendances(self):
         return filter(lambda attendance: attendance.waiting, self.attendances)
 
-    def get_active_applicants(self):
+    def get_active_attendances(self):
         return filter(lambda attendance: not attendance.waiting, self.attendances)
 
-    def get_paying_applicants(self):
+    def get_paying_attendances(self):
         return filter(lambda attendance: not attendance.waiting and attendance.has_to_pay, self.attendances)
 
-    def get_free_applicants(self):
+    def get_free_attendances(self):
         return filter(lambda attendance: not attendance.waiting and not attendance.has_to_pay, self.attendances)
+
+    def restock(self):
+        # Negative number of free seats may happen if s.o. manually added an attendance
+        num_free = max(self.limit - len(self.get_active_attendances()), 0)
+        waiting = self.get_waiting_attendances()
+
+        to_move = waiting[:num_free]
+
+        # Check if an applicant has to pay before toggling the waiting status,
+        # otherwise he has to pay _from the status change_ on.
+        for attendance in to_move:
+            attendance.has_to_pay = attendance.applicant.has_to_pay()
+            attendance.waiting = False
 
 
 class Language(db.Model):

spz/__init__.py
@@ -106,6 +106,8 @@ routes = [('/', views.index, ['GET', 'POST']),
           ('/internal/payments', views.payments, ['GET', 'POST']),
           ('/internal/status/<int:applicant_id>/<int:course_id>', views.status, ['GET', 'POST']),
 
+          ('/internal/restock', views.restock, ['GET', 'POST']),
+
           ('/internal/statistics', views.statistics, ['GET']),
           ('/internal/statistics/free_courses', views.free_courses, ['GET']),
 

spz/forms.py
@@ -165,7 +165,7 @@ class NotificationForm(Form):
     def get_courses(self):
         return models.Course.query.filter(models.Course.id.in_(self.mail_courses.data)).all()
 
-    # TODO: refactor by using the course's get_active_applicants member function
+    # TODO: refactor by using the course's get_active_attendances member function
     def get_recipients(self):
         flatten = lambda x: sum(x, [])
         attendances = flatten([course.attendances for course in self.get_courses()]) # single list of attendances
@@ -279,6 +279,17 @@ class SearchForm(Form):
     """
 
     token = TextField(u'Suchen', [validators.Required(u'Suchparameter muss angegeben werden')])
+
+
+class RestockForm(Form):
+    """Represents a form to fill languages and courses with waiting applicants.
+    """
+
+    language = SelectField(u'Sprache', [validators.Required(u'Die Sprache muss angegeben werden')], coerce=int,
+                           choices=[(language.id, language.name) for language in models.Language.query.order_by(models.Language.name).all()])
+
+    def get_courses(self):
+        return models.Language.query.get(self.language.data).courses.all()
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:

spz/templates/internal/internal.html
@@ -12,7 +12,8 @@
 <li data-intro='Zur Bearbeitung einzelner Zahlungsvorgänge' data-step='7'><a href="{{ url_for('payments') }}"><span class="glyphicon glyphicon-euro"></span> Zahlungen</a></li>
 <li data-intro='Informative Statistiken zur Kursbelegung' data-step='8'><a href="{{ url_for('statistics') }}"><span class="glyphicon glyphicon-book"></span> Statistiken</a></li>
 <li data-intro='Zum Finden von Teilnehmern mit mehrfachen freien Kursteilnahmen' data-step='9'><a href="{{ url_for('duplicates') }}"><span class="glyphicon glyphicon-random"></span> Doppelgänger</a></li>
-<li data-intro='Wir sind für jede Art von Feedback dankbar' data-step='10'><a href="mailto:daniel+spz@trvx.org?subject=[SPZ] Feedback&cc=roman@zukowsky.de"><span class="glyphicon glyphicon-thumbs-up"></span> Feedback</a></li>
+<li data-intro='Automatisches Holen von Nachrücker, pro Sprache' data-step='10'><a href="{{ url_for('restock') }}"><span class="glyphicon glyphicon-repeat"></span> Nachrücker</a></li>
+<li data-intro='Wir sind für jede Art von Feedback dankbar' data-step='11'><a href="mailto:daniel+spz@trvx.org?subject=[SPZ] Feedback&cc=roman@zukowsky.de"><span class="glyphicon glyphicon-thumbs-up"></span> Feedback</a></li>
 {% endif %}
 {% endblock navbar %}
 

spz/templates/internal/language.html
@@ -35,11 +35,11 @@ Sprache: {{ language.name }}
                 <tr class="{% if course.is_overbooked() %}danger{% elif course.is_full() %}warning{% endif %}">
                     <td><a href="{{ url_for('course', id=course.id) }}"><strong>{{ course.language.name }} {{ course.level }}</strong></a></td>
                     <td>{{ course.limit }}</td>
-                    <td>{{ course.limit - course.get_active_applicants()|count }}</td>
+                    <td>{{ course.limit - course.get_active_attendances()|count }}</td>
                     <td>{{ course.attendances|count }}</td>
-                    <td>{{ course.get_active_applicants()|count }}</td>
-                    <td>{{ course.get_waiting_applicants()|count }}</td>
-                    <td>{{ course.get_paying_applicants()|count }}</td>
+                    <td>{{ course.get_active_attendances()|count }}</td>
+                    <td>{{ course.get_waiting_attendances()|count }}</td>
+                    <td>{{ course.get_paying_attendances()|count }}</td>
                 </tr>
                 {% endfor %}
             </tbody>

spz/templates/internal/restock.html
@@ -1,0 +1,31 @@
+{% extends 'internal/internal.html' %}
+{% from 'formhelpers.html' import render_option %}
+
+{% block caption %}
+Nachrücker
+{% endblock caption %}
+
+{% block internal_body %}
+<div class="row">
+    <div class="col-lg-8 col-lg-offset-2">
+        <div class="well">
+            <form class="form-horizontal" method="post">
+                {{ form.hidden_tag() }}
+                <fieldset>
+                    <legend>Fülle Restplätze folgender Sprache</legend>
+                    {{ render_option(form.language, help='Basierend auf sequentiellen Eintreffen der Teilnahmen.') }}
+                </fieldset>
+                <fieldset>
+                    <div class="form-group">
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <button type="submit" class="btn btn-primary">Abschicken</button>
+                            <button type="reset"  class="btn btn-default">Zurücksetzen</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</div>
+{% endblock internal_body %}
+{# vim: set tabstop=4 shiftwidth=4 expandtab: #}

spz/templates/internal/statistics/free_courses.html
@@ -19,12 +19,12 @@ Restplätze
             </thead>
             <tbody>
                 {% for course in courses %}
-                    {% if course.limit > course.get_active_applicants()|count %}
+                    {% if course.limit > course.get_active_attendances()|count %}
                     <tr>
                         <td><a href="{{ url_for('course', id=course.id ) }}">{{ course.language.name }} {{ course.level }}</a></td>
                         <td>{{ course.limit }}</td>
-                        <td>{{ course.limit - course.get_active_applicants()|count }}</td>
-                        <td>{{ course.get_waiting_applicants()|count }}</td>
+                        <td>{{ course.limit - course.get_active_attendances()|count }}</td>
+                        <td>{{ course.get_waiting_attendances()|count }}</td>
                     </tr>
                     {% endif %}
                 {% endfor %}

spz/views.py
@@ -17,7 +17,7 @@ from flask.ext.mail import Message
 
 from spz import app, models, mail, db
 from spz.decorators import templated, auth_required
-from spz.forms import SignupForm, NotificationForm, ApplicantForm, StatusForm, PaymentForm, SearchForm
+from spz.forms import SignupForm, NotificationForm, ApplicantForm, StatusForm, PaymentForm, SearchForm, RestockForm
 
 
 @templated('signup.html')
@@ -382,6 +382,29 @@ def duplicates():
     doppelganger = [models.Applicant.query.filter_by(tag=duptag).all() for duptag in map(lambda tup: tup[0], taglist)]
 
     return dict(doppelganger=doppelganger)
+
+
+@auth_required
+@templated('internal/restock.html')
+def restock():
+    form = RestockForm()
+
+    if form.validate_on_submit():
+        courses = form.get_courses()
+
+        try:
+            for course in courses:
+                course.restock()
+
+            db.session.commit()
+            flash(u'Kurse bestmöglichst mit Nachrückern gefüllt', 'success')
+
+            return redirect(url_for('internal'))
+        except Exception as e:
+            db.session.rollback()
+            flash(u'Die Kurse konnten nicht mit Nachrückern gefüllt werden: {0}'.format(e), 'danger')
+
+    return dict(form=form)
 
 
 # vim: set tabstop=4 shiftwidth=4 expandtab:

commit bb93d7a1011e38736a5fc0099614390ebd007e75

Fixed Course.is_full() regression, introduced by d71e225d

spz/models.py
@@ -188,7 +188,7 @@ class Course(db.Model):
         return self.rating_lowest <= applicant.best_rating() <= self.rating_highest
 
     def is_full(self):
-        return len(self.get_active_attendances()) >= self.limit
+        return len(self.attendances) >= self.limit
 
     def is_overbooked(self):
         return len(self.attendances) >= (self.limit * app.config['OVERBOOKING_FACTOR'])