d211115125e396680e6204769dd19f3855ecec5e
Felix Rittler <felix.rittler@web.de>
2017-03-21 16:38:52 +0100

    spz: edited authors
    Signed-off-by: Felix Rittler <felix.rittler@web.de>

--- images/spz/doc/conf.py
+++ images/spz/doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'spz-signup'
-copyright = u'2013-2016, Roman Zukowsky, Daniel Hofmann, Marco Neumann'
+copyright = u'2013-2017, Roman Zukowsky, Daniel Hofmann, Marco Neumann, Felix Rittler'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -187,7 +187,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'spz-signup.tex', u'spz-signup Documentation',
-   u'Roman Zukowsky, Daniel Hofmann, Marco Neumann', 'manual'),
+   u'Roman Zukowsky, Daniel Hofmann, Marco Neumann, Felix Rittler', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -217,7 +217,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'spz-signup', u'spz-signup Documentation',
-     [u'Roman Zukowsky, Daniel Hofmann, Marco Neumann'], 1)
+     [u'Roman Zukowsky, Daniel Hofmann, Marco Neumann, Felix Rittler'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -231,7 +231,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'spz-signup', u'spz-signup Documentation',
-   u'Roman Zukowsky, Daniel Hofmann, Marco Neuman', 'spz-signup',
+   u'Roman Zukowsky, Daniel Hofmann, Marco Neuman, Felix Rittler', 'spz-signup',
    'One line description of project.',
    'Miscellaneous'),
 ]
