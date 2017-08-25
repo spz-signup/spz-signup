0a52c178fcd67e76bc01209fdcd69fcd73e5b709
Felix Rittler <felix.rittler@web.de>
2017-08-25 15:11:07 +0200

    spz: confirmation of changes didn't work -> replaced it by checkbox
    Signed-off-by: Felix Rittler <felix.rittler@web.de>

--- images/spz/spz/static/js/internal.js
+++ images/spz/spz/static/js/internal.js
@@ -2,11 +2,4 @@ $(document).ready(function(){
     'use strict';
 
     $('.ui.table.sortable').tablesort();
-
-    $('button[data-confirm]').each(function() {
-        var msg = $(this).data('confirm');
-        $(this).click(function() {
-            confirm(msg);
-        });
-    });
 });
