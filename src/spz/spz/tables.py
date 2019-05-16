ce7b03ab93d4d362449a950488ee570782b6ee3e
Tims777 <timzi777@gmail.com>
2019-05-16 11:02:34 +0200

    Make xlsx export use a writer class too

--- src/spz/spz/tables.py
+++ src/spz/spz/tables.py
@@ -10,14 +10,14 @@ import io
 from tempfile import NamedTemporaryFile
 from openpyxl import Workbook
 
-from flask import make_response, send_file, url_for, redirect, flash
+from flask import make_response, url_for, redirect, flash
 
 
 def export_course_list(courses, format):
     if format == 'csv':
-        return csv_export(courses)
+        return export(CSVWriter(), courses)
     elif format == 'xlsx':
-        return excel_export(courses)
+        return export(ExcelWriter(), courses)
     else:
         flash('Ungueltiges Export-Format: {0}'.format(format), 'error')
         return redirect(url_for('lists'))
@@ -30,20 +30,41 @@ class CSVWriter:
     def __init__(self):
         self.buf = io.StringIO()
         self.out = csv.writer(self.buf, delimiter=";", dialect=csv.excel)
+        self.mimetype = 'text/csv'
+        self.filename = 'Kursliste.csv'
 
     def write_heading(self, values):
         self.write_row(values)
 
     def write_row(self, values):
         self.out.writerow(values)
-    
+
     def get_data(self):
         return self.buf.getvalue()
 
 
-def csv_export(courses):
-    writer = CSVWriter()
+class ExcelWriter:
 
+    def __init__(self):
+        self.workbook = Workbook()
+        self.mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        self.filename = 'Kursliste.xlsx'
+
+    def write_heading(self, values):
+        self.write_row(values)
+
+    def write_row(self, values):
+        self.workbook.active.append(values)
+
+    def get_data(self):
+        with NamedTemporaryFile() as file:
+            self.workbook.save(file.name)
+            file.seek(0)
+            stream = file.read()
+        return stream
+
+
+def export(writer, courses):
     # XXX: header -- not standardized
     writer.write_heading(['Kurs', 'Kursplatz', 'Bewerbernummer', 'Vorname', 'Nachname', 'Mail',
                   'Matrikelnummer', 'Telefon', 'Studienabschluss', 'Semester', 'Bewerberkreis'])
@@ -58,33 +79,20 @@ def csv_export(courses):
         idx = 1
         for applicant in active_no_debt:
             writer.write_row(['{0}'.format(course.full_name()),
-                              '{0}'.format(idx),
-                              '{0}'.format(applicant.id),
-                              applicant.first_name,
-                              applicant.last_name,
-                              applicant.mail,
-                              maybe(applicant.tag),
-                              maybe(applicant.phone),
-                              applicant.degree.name if applicant.degree else '',
-                              '{0}'.format(maybe(applicant.semester)),
-                              applicant.origin.name if applicant.origin else ''])
+                                  '{0}'.format(idx),
+                                  '{0}'.format(applicant.id),
+                                  applicant.first_name,
+                                  applicant.last_name,
+                                  applicant.mail,
+                                  maybe(applicant.tag),
+                                  maybe(applicant.phone),
+                                  applicant.degree.name if applicant.degree else '',
+                                  '{0}'.format(maybe(applicant.semester)),
+                                  applicant.origin.name if applicant.origin else ''])
             idx += 1
 
     resp = make_response(writer.get_data())
-    resp.headers['Content-Disposition'] = 'attachment; filename="Kursliste.csv"'
+    resp.headers['Content-Disposition'] = 'attachment; filename="{0}"'.format(writer.filename)
     resp.mimetype = writer.mimetype
 
-    return resp
-
-
-def excel_export(courses):
-    wb = Workbook()
-    wb.active.title = "Kursliste"
-    with NamedTemporaryFile() as file:
-        wb.save(file.name)
-        file.seek(0)
-        stream = file.read()
-        resp = make_response(stream)
-        resp.headers['Content-Disposition'] = 'attachment; filename="Kursliste.xlsx"'
-        resp.mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     return resp
