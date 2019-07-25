de7200b6b09387f712bc269ff0d05494e814f8b1
Tims777 <timzi777@gmail.com>
2019-07-25 17:58:35 +0200

    Add basic pytest integration

---
+++ src/spz/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""Testing resources.
+
+   See https://pytest.org/ for information about automatic test-discovery.
+"""
---
+++ src/spz/tests/test_views.py
@@ -0,0 +1,17 @@
+import pytest
+
+from spz import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    #with app.app_context():
+    #    app.init_db()
+
+    yield client
+
+def test_startpage(client):
+    assert client.get('/').status_code == 200
