17ce94f4990017ed766d5b00e4c57f6f6cd1aeb8
Tims777 <timzi777@gmail.com>
2020-03-28 12:48:46 +0100

    Setup automatic translation-building

--- src/spz/util/collect_translations.sh
+++ src/spz/util/collect_translations.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-pybabel extract -F i18n.build/babel.cfg -o i18n.build/messages.pot .
+pybabel extract -F i18n.build/babel.conf -o i18n.build/messages.pot .
+# pybabel init -i i18n.build/messages.pot -d i18n.build -l en
+pybabel update -i i18n.build/messages.pot -d i18n.build
+pybabel compile -d i18n.build
